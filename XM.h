433f91ddce43bdae4a8d2f0548f61c8cddabe867
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2019-04-08 10:32:34 +0300

    type fix for clock

--- XM.h
+++ XM.h
@@ -12,17 +12,17 @@ class XM
 {
 public:
 
-	std::chrono::steady_clock::time_point swarmStart;
+	std::chrono::high_resolution_clock::time_point swarmStart;
 	float swarmDuration;
 
 	XM();
 	~XM();
 
-	void startSwarm(std::chrono::steady_clock::time_point tp) {
+	void startSwarm(std::chrono::high_resolution_clock::time_point tp) {
 		swarmStart = tp;
 	}
 
-	void endSwarm(std::chrono::steady_clock::time_point tp) {
+	void endSwarm(std::chrono::high_resolution_clock::time_point tp) {
 		swarmDuration = std::chrono::duration_cast<std::chrono::microseconds>(tp - swarmStart).count() / 1000000.f;
 	}
 
